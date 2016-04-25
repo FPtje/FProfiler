c1076342b7c4467349a270290d9e0a726e745124
FPtje <fpeijnenburg@gmail.com>
2016-04-25 20:33:56 +0200

    Fixed lag by limiting output

--- lua/fprofiler/ui/clientcontrol.lua
+++ lua/fprofiler/ui/clientcontrol.lua
@@ -21,7 +21,28 @@ Stop profiling
 local function stopProfiling()
     FProfiler.stop()
 
-    update({"client", "bottlenecks"}, FProfiler.mostTimeInclusive())
+    -- Prevent the model from getting too many things
+    local dict = {}
+    local mostTime = FProfiler.mostTimeInclusive(100)
+    for i = 1, #mostTime do dict[mostTime[i].func] = true end
+
+    local mostAvg = FProfiler.mostTimeInclusiveAverage(100)
+
+    for i = 1, #mostAvg do
+        if dict[mostAvg[i].func] then continue end
+        dict[mostAvg[i].func] = true
+        table.insert(mostTime, mostAvg[i])
+    end
+
+    local mostCalled = FProfiler.mostOftenCalled(100)
+
+    for i = 1, #mostCalled do
+        if dict[mostCalled[i].func] then continue end
+        dict[mostCalled[i].func] = true
+        table.insert(mostTime, mostCalled[i])
+    end
+
+    update({"client", "bottlenecks"}, mostTime)
     update({"client", "topLagSpikes"}, FProfiler.getMostExpensiveSingleCalls())
 
     local newTime = get({"client", "recordTime"}) + CurTime() - (get({"client", "sessionStart"}) or 0)
